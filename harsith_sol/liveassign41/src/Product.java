ab07e9b063bab1d467a5a941846ca0e18d278db1
rajan <rajansah0020@gmail.com>
2025-01-29 15:39:45 +0100

    adding new code

--- harsith_sol/liveassign41/src/Product.java
+++ harsith_sol/liveassign41/src/Product.java
@@ -1,2 +1,35 @@
 public class Product {
+
+    String name;
+    String brandName;
+    double price;
+
+    Product(String name,String brandName, double price)
+    {
+      this.name=name;
+      this.brandName=brandName;
+      this.price=price;
+    }
+
+    Product(String name,double price)
+    {this.name=name;
+        this.brandName="unknown";
+        this.price=price;
+
+    }
+
+    void setName(String name)
+    {
+        this.name=name;
+    }
+
+    void setBrandName(String brandName)
+    {
+        this.brandName=brandName;
+    }
+
+    void setPrice(String price)
+    {
+        this.price=price;
+    }
 }
