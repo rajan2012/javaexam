feec0d3ab0d12a9c9c945c2c31e354960e5e5a04
rajan <rajansah0020@gmail.com>
2025-02-05 16:07:17 +0100

    adding new code

---
+++ harsith_sol/assign13q2/src/main3.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class main3 {
+
+    public static void main(String[] args) {
+        int[] d=new int[]{0,1,1};
+
+        System.out.println(Arrays.toString(d));
+    }
+}
