3c63ce5e275616dd30b476af5f2dcc609e1f8297
rajan <rajansah0020@gmail.com>
2025-02-15 22:04:08 +0100

    adding core concept

---
+++ harsith_sol/stuff2/src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        String s="Streetname 12 1234 City";
+
+        String[] s1=s.split(",");
+
+        System.out.println(s1.length);
+
+
+        }
+    }
